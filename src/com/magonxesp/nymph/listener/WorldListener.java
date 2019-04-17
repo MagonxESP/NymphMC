6427b479af8bf257ed2fa2046b8c38f357796763
MagonxESP <janma.360@gmail.com>
2019-04-17 15:14:49 +0200

    cada 1 hora envia el progreso del preprocesado del mundo

--- src/com/magonxesp/nymph/listener/WorldListener.java
+++ src/com/magonxesp/nymph/listener/WorldListener.java
@@ -1,6 +1,7 @@
 package com.magonxesp.nymph.listener;
 
 import com.magonxesp.nymph.Nymph;
+import com.magonxesp.nymph.scheduler.WorldBorderFillProgressScheduler;
 import com.wimbli.WorldBorder.Events.WorldBorderFillFinishedEvent;
 import com.wimbli.WorldBorder.Events.WorldBorderFillStartEvent;
 import org.bukkit.event.EventHandler;
@@ -8,14 +9,25 @@ import org.bukkit.event.Listener;
 
 public class WorldListener implements Listener {
 
+    private int progressTaskId = 0;
+
     @EventHandler
     public void onWorldBorderFillStart(WorldBorderFillStartEvent event) {
         Nymph.broadcastMessage("Se ha iniciado el preprocesado del mundo " + event.getFillTask().refWorld(), true);
+        progressTaskId = Nymph.getPlugin()
+                .getServer()
+                .getScheduler()
+                .scheduleSyncRepeatingTask(Nymph.getPlugin(), new WorldBorderFillProgressScheduler(event), 0, 60 * 60 * 20);
+
     }
 
     @EventHandler
     public void onWorldBorderFillEnd(WorldBorderFillFinishedEvent event) {
         Nymph.broadcastMessage("Se ha terminado el preprocesado del mundo " + event.getWorld().getName(), true);
+
+        if (progressTaskId != 0) {
+            Nymph.getPlugin().getServer().getScheduler().cancelTask(progressTaskId);
+        }
     }
 
 }
