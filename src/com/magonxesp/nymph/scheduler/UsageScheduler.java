05459d228b486fb6c393b92fbda0b2745fcd4bbb
MagonxESP <janma.360@gmail.com>
2019-04-17 23:47:01 +0200

    ram usage alert fix

--- src/com/magonxesp/nymph/scheduler/UsageScheduler.java
+++ src/com/magonxesp/nymph/scheduler/UsageScheduler.java
@@ -19,11 +19,11 @@ public class UsageScheduler implements Runnable {
     private void alertOnLowMemory() {
         float free = getMemoryFree();
 
-        if (free < 512 && !lowRamUsageAlerted) {
+        if (free < 250 && !lowRamUsageAlerted) {
             String msg = "Queda poca memoria ram en el servidor (" + free + "MB libre)";
             Nymph.broadcastMessage(msg, false);
             lowRamUsageAlerted = true;
-        } else if (free > 512 && lowRamUsageAlerted) {
+        } else if (free > 2048 && lowRamUsageAlerted) {
             String msg = "El servidor vuelve a tener ram UwU";
             Nymph.broadcastMessage(msg, false);
             lowRamUsageAlerted = false;
