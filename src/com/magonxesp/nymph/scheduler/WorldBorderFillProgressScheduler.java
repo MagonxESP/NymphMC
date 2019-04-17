d1d59315392ac151dccdcdbab9e9593080b6514e
MagonxESP <janma.360@gmail.com>
2019-04-17 15:16:34 +0200

    runnable que alerta del progreso del preprocesador

---
+++ src/com/magonxesp/nymph/scheduler/WorldBorderFillProgressScheduler.java
@@ -0,0 +1,23 @@
+package com.magonxesp.nymph.scheduler;
+
+import com.magonxesp.nymph.Nymph;
+import com.wimbli.WorldBorder.Events.WorldBorderFillStartEvent;
+
+public class WorldBorderFillProgressScheduler implements Runnable {
+
+    private WorldBorderFillStartEvent event;
+
+    public WorldBorderFillProgressScheduler(WorldBorderFillStartEvent event) {
+        this.event = event;
+    }
+
+    @Override
+    public void run() {
+        double completed = event.getFillTask().getPercentageCompleted();
+        String world = event.getFillTask().refWorld();
+
+        if ((completed % 10) == 0) {
+            Nymph.broadcastMessage("El preprocesado de " + world + " esta al " + completed + "% completado", true);
+        }
+    }
+}
