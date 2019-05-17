60eb473f4587fe35c14e622a22c5e423e9ec2f63
MagonxESP <janma.360@gmail.com>
2019-05-17 22:56:43 +0200

    Nymph NPC

--- src/main/java/com/magonxesp/nymph/npc/NymphEntity.java
+++ src/main/java/com/magonxesp/nymph/npc/NymphEntity.java
@@ -1,33 +1,51 @@
 package com.magonxesp.nymph.npc;
 
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.magonxesp.nymph.Nymph;
+import com.magonxesp.nymph.http.HttpRequest;
 import com.mojang.authlib.GameProfile;
-import net.minecraft.server.v1_13_R2.EntityPlayer;
-import net.minecraft.server.v1_13_R2.MinecraftServer;
-import net.minecraft.server.v1_13_R2.PlayerInteractManager;
-import net.minecraft.server.v1_13_R2.WorldServer;
+import com.mojang.authlib.properties.Property;
+import net.minecraft.server.v1_13_R2.*;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_13_R2.entity.CraftPlayer;
+import org.bukkit.entity.Player;
 
+import java.io.IOException;
 import java.util.UUID;
 
 public class NymphEntity {
 
     private EntityPlayer entityPlayer;
+    private GameProfile gameProfile;
+    private PlayerInteractManager playerInteractManager;
+    private MinecraftServer minecraftServer;
+    private WorldServer worldServer;
 
     public NymphEntity(MinecraftServer minecraftserver, WorldServer worldserver) {
-        GameProfile gameProfile = new GameProfile(UUID.randomUUID(), "Nymph");
-        PlayerInteractManager playerInteractManager = new PlayerInteractManager(worldserver);
+        this.gameProfile = new GameProfile(UUID.randomUUID(), "Nymph");
+        this.playerInteractManager = new PlayerInteractManager(worldserver);
+        this.minecraftServer = minecraftserver;
+        this.worldServer = worldserver;
+        this.setSkin();
+    }
 
-        entityPlayer = new EntityPlayer(minecraftserver, worldserver, gameProfile, playerInteractManager);
+    private void setSkin() {
+        gameProfile.getProperties()
+                .put("textures", new Property("textures",
+                        "eyJ0aW1lc3RhbXAiOjE1NTgxMjYxMzI3MTEsInByb2ZpbGVJZCI6IjNmYzdmZGY5Mzk2MzRjNDE5MTE5OWJhM2Y3Y2MzZmVkIiwicHJvZmlsZU5hbWUiOiJZZWxlaGEiLCJzaWduYXR1cmVSZXF1aXJlZCI6dHJ1ZSwidGV4dHVyZXMiOnsiU0tJTiI6eyJ1cmwiOiJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlL2E0ZmRiMjE2ZTJkYTIyZGVlN2Q1ZTU2ZGJjNTg0Mzc2ODFlY2U1MmFlMjc5NTg1MTAyN2ExMGNjODY0ZDVjYWYifX19",
+                        "yg0uHgH0fBecX7I3ht7asCMJWwKpZhJGCwmVu0VODKNmR5BF7tBfkgPc/P1D/HFeDW4FxpZdUYOypP6yl3E/KPenH1aT/jAkG4L7DRvRyRjZZDzW310K5WK2YSihTEFO5VEyrXhQ+vMdh7iCYZo8uHtppUfJOCeb4ogze/m9ne9JtDMRrgKBhCrF0rnVGmqq2V5BG1erP2lLJ8qI5HMNS4ItNrmN8HJcxVHssceeVW5IoulwVdlXLtT0wudktur4DVLbS+c/g4Y5+c3zUhLU4Uk8wnD94HJ2wu+S1bmZPYbHZND8zgomg/0zCjTzRehQgs4rRqC/AVMpf7DTBtiA44LGeMpeecbg+FCS+uCWhNg8NwasdB3uwRpdcoAYkIWVWccxazrr0P91CYqm48b8AUJKB5qNJZJpXpjbV7j06FwzqSWVXXeWLmv50Icso9HjEkiRtmKKy2vytD/2cc3EsAdMmdtOFVrKePc4GXBe8v/Re0LQDo1PqDjaZ1QGp6sHduW43zX9dgwbDSRM+ZBfg1axo53O8axNiXJCFzyu5fQImqiTpqVDi9AY6D9RKrp2QUSiX6BSjHoV+q6N5qpqcZYP7/fFU0qII8HrIPb4RTvnh9h9aLIIVHNuC59uZk0mnofPWSs7QU8i6LyrAZMa55QPOMms9xe/Mmp2Qy41BBs="));
+
+    }
+
+    public void spawn(Player player, Location location) {
+        entityPlayer = new EntityPlayer(minecraftServer, worldServer, gameProfile, playerInteractManager);
         entityPlayer.setInvulnerable(true);
-    }
+        entityPlayer.teleportTo(location, false);
 
-    public void spawn(Location location) {
-        entityPlayer.setLocation(location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
-        entityPlayer.spawnIn(entityPlayer.world);
-    }
-
-    public EntityPlayer getEntity() {
-        return entityPlayer;
+        PlayerConnection connection = ((CraftPlayer) player).getHandle().playerConnection;
+        connection.sendPacket(new PacketPlayOutPlayerInfo(PacketPlayOutPlayerInfo.EnumPlayerInfoAction.ADD_PLAYER, entityPlayer));
+        connection.sendPacket(new PacketPlayOutNamedEntitySpawn(entityPlayer));
     }
 }
