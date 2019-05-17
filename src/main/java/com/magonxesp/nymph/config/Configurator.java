ec0879d4bf215930073aea1355c3f8e9a882a6be
MagonxESP <janma.360@gmail.com>
2019-05-17 18:58:24 +0200

    update .gitignore

--- src/main/java/com/magonxesp/nymph/config/Configurator.java
+++
@@ -1,9 +0,0 @@
-package com.magonxesp.nymph.config;
-
-public class Configurator {
-
-    public Configurator(String configFilePath) {
-
-    }
-
-}
--- src/main/java/com/magonxesp/nymph/config/Parameters.java
+++
@@ -1,5 +0,0 @@
-package com.magonxesp.nymph.config;
-
-public class Parameters {
-    private static final int num = 861;
-}
