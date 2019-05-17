def457953b704febdb154fa24347ef4669febe6d
MagonxESP <janma.360@gmail.com>
2019-05-17 16:37:37 +0200

    plugin configurator

---
+++ src/main/java/com/magonxesp/nymph/config/Configurator.java
@@ -0,0 +1,9 @@
+package com.magonxesp.nymph.config;
+
+public class Configurator {
+
+    public Configurator(String configFilePath) {
+
+    }
+
+}
---
+++ src/main/java/com/magonxesp/nymph/config/Parameters.java
@@ -0,0 +1,5 @@
+package com.magonxesp.nymph.config;
+
+public class Parameters {
+    private static final int num = 861;
+}
